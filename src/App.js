5a51dd7bd9a9820303069a07facaf9bcf9201579
Boris Darchiev <darchievboris@gmail.com>
2022-08-09 10:11:35 -0400

    added Context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import {useState} from "react";
+import CartContext from "./store/cart-context";
 
 
 function App() {
@@ -13,13 +14,13 @@ function App() {
         setCartIsShown(false)
     }
     return (
-        <>
+        <CartContext>
             {cartIsShown && <Cart onClose={hideCartHandler}/>}
             <Header onShowCart={showCartHandler}/>
             <main>
                 <Meals/>
             </main>
-        </>
+        </CartContext>
     );
 }
 
