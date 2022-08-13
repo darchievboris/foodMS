622cc957d01389eac4540c6c0da9934fbe436d89
Boris Darchiev <darchievboris@gmail.com>
2022-08-13 13:14:22 -0400

    added functionality for adding duplicate items from the main list to the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,6 +7,7 @@ import CartItem from "./CartItem";
 
 const Cart = (props) => {
     const cartCtx = useContext(CartContext);
+    console.log(cartCtx.totalAmount)
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
     const hasItems = cartCtx.items.length > 0
     const cartItemRemoveHandler = id => {
@@ -15,24 +16,24 @@ const Cart = (props) => {
     }
 
     return (<Modal onClose={props.onClose}>
-            <ul className={classes['cart-items']}>
-                {cartCtx.items.map(item => <CartItem
-                    key={item.id}
-                    name={item.name}
-                    amount={item.amount}
-                    price={item.price}
-                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
-                    onAdd={cartItemAddHandler.bind(null, item)}/>)}
-            </ul>
-            <div className={classes.total}>
-                <span>Total Amount</span>
-                <span>{totalAmount}</span>
-            </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
-                {hasItems && <button className={classes.button}>Order</button>}
-            </div>
-        </Modal>)
+        <ul className={classes['cart-items']}>
+            {cartCtx.items.map(item => <CartItem
+                key={item.id}
+                name={item.name}
+                amount={item.amount}
+                price={item.price}
+                onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                onAdd={cartItemAddHandler.bind(null, item)}/>)}
+        </ul>
+        <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>{totalAmount}</span>
+        </div>
+        <div className={classes.actions}>
+            <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
+            {hasItems && <button className={classes.button}>Order</button>}
+        </div>
+    </Modal>)
 }
 
 export default Cart;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,8 +7,23 @@ const defaultCartState = {
 }
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const updateItems = state.items.concat(action.item)
-        const updatedTotalAmount = state.totalAmount + action.item.price * action.amount
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id)
+        const existingCartItem = state.items[existingCartItemIndex]
+        // debugger
+        let updateItems
+        if (existingCartItem) {
+            const updateItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+            updateItems = [...state.items]
+            updateItems[existingCartItemIndex] = updateItem
+        } else {
+            updateItems = state.items.concat(action.item)
+        }
+
         return {
             items: updateItems,
             totalAmount: updatedTotalAmount
