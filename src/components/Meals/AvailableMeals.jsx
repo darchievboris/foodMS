ea24ff6532d81cccb2ca0263183a0df3538914c9
Boris Darchiev <darchievboris@gmail.com>
2022-08-17 10:15:46 -0400

    added connection to firebase and pulled data from db

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import classes from './AvailableMeals.module.css'
 import MealItem from "./MealItem/MealItem";
 import Card from "../UI/Card";
 
-const DUMMY_MEALS = [{
-    id: 'm1', name: 'Sushi', description: 'Finest fish and veggies', price: 22.99,
-}, {
-    id: 'm2', name: 'Schnitzel', description: 'A german specialty!', price: 16.5,
-}, {
-    id: 'm3', name: 'Barbecue Burger', description: 'American, raw, meaty', price: 12.99,
-}, {
-    id: 'm4', name: 'Green Bowl', description: 'Healthy...and green...', price: 18.99,
-},];
 const AvailableMeals = () => {
+    const [meals, setMeals] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchMealsHandler = (async () => {
+        setIsLoading(true)
+        setError(null)
+        try {
+            const response = await fetch('https://foodms-6bf9d-default-rtdb.firebaseio.com/meals.json')
+            if (!response.ok) throw new Error('Something went wrong!')
+
+            const data = await response.json()
+            const loadedMeals = []
+            for (const key in data) {
+                loadedMeals.push({
+                    id: key,
+                    name: data[key].name,
+                    description: data[key].description,
+                    price: data[key].price
+                })
+            }
+            setMeals(loadedMeals)
+        } catch (error) {
+            setError(error.message)
+        }
+        setIsLoading(false)
+    }, [])
+    useEffect(() => {
+        fetchMealsHandler()
+    }, []);
+
+    let content = <p>Found no meals</p>
+
+    if (meals.length > 0) {
+        content = meals.map(meal =>
+            <MealItem
+                key={meal.id}
+                id={meal.id}
+                name={meal.name}
+                description={meal.description}
+                price={meal.price}/>
+        )
+    }
+    if (error) {
+        content = <p>{error}</p>
+    }
+    if (isLoading) {
+        content = <p>Loading...</p>
+    }
+
     return (<section className={classes.meals}>
         <Card>
             <ul>
-                {DUMMY_MEALS.map(meal =>
-                    <MealItem
-                        key={meal.id}
-                        id={meal.id}
-                        name={meal.name}
-                        description={meal.description}
-                        price={meal.price}/>
-                )}
+                {content}
             </ul>
         </Card>
     </section>);
